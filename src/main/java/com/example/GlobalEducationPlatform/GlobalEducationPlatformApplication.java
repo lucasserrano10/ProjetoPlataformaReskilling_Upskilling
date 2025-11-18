021b4dbe921b91dbeca5e950cf658c87e8466ea1
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-11-18 18:44:10 -0300

    adding the structure of the project

---
+++ src/main/java/com/example/GlobalEducationPlatform/GlobalEducationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.GlobalEducationPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalEducationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalEducationPlatformApplication.class, args);
+	}
+
+}
